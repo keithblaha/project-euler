6fda20cb70b67e3f737fcbdcd98566523748fef9
Keith Blaha <keith.blaha@gmail.com>
2015-02-20 08:56:49 -0800

    problem 1

---
+++ 1/Main.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+  // it'd probably be faster to generate the multiples rather than filter down from all possibilties... oh well
+  val solution = (1 until 1000).filter(x => x % 3 == 0 || x % 5 == 0).reduce(_ + _)
+  Console.println(solution)
+}
