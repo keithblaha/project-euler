d48808522f0acc0141ca01d449518198a5ac4fe5
Keith Blaha <keith.blaha@gmail.com>
2015-02-24 18:58:36 -0800

    4

---
+++ 4/Main.scala
@@ -0,0 +1,19 @@
+object Main extends App {
+  def isPalindrome(s: String): Boolean =  {
+    if (s.length <= 1) return true
+    else if (s.head == s.last) return isPalindrome(s.slice(1, s.length-1))
+    else return false
+  }
+
+  var max = -1
+  val solution = {
+    val space = (100 until 1000).foreach(x => {
+      (100 until 1000).foreach(y => {
+        val n = x * y
+        if(isPalindrome(n.toString) && n > max) max = n
+      })
+    })
+    max
+  }
+  Console.println(solution)
+}
