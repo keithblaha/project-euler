b1708aab06dad91358b0f64190502fd65c96dfb3
Keith Blaha <keith.blaha@gmail.com>
2015-02-25 09:19:21 -0800

    6

---
+++ 6/Main.scala
@@ -0,0 +1,19 @@
+object Main extends App {
+  def sumOfSquares(n: Int) = {
+    // naive:
+    // (1 to n).map(x => x*x).sum
+    // better:
+    n * (n + 1) * (2*n + 1) / 6
+  }
+  def squareOfSums(n: Int) = {
+    // naive:
+    // val s = (1 to n).sum
+    // better:
+    val s = n * (n + 1) / 2
+    s * s
+  }
+  val solution = {
+    squareOfSums(100) - sumOfSquares(100) 
+  }
+  Console.println(solution)
+}
