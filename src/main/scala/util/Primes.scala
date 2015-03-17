00ee1b5906d751514516a1b165e6f14612248049
Keith Blaha <keith.blaha@gmail.com>
2015-03-17 08:36:11 -0700

    ignore evens in sieve of eratosthenes

--- src/main/scala/util/Primes.scala
+++ src/main/scala/util/Primes.scala
@@ -4,17 +4,18 @@ import scala.collection.mutable.Buffer
 
 object Primes {
   def generatePrimesTo(limit: Int) = {
-    // Sieve of Eratosthenes
+    // use optimized Sieve of Eratosthenes
     val isPrimeMap = scala.collection.mutable.Map[Int, Boolean]()
-    val nums = (2 to limit)
-    val primes = Buffer[Int]()
+    val primes = Buffer[Int](2)
 
-    nums.foreach(n => {
+    // only need to look at odds
+    (3 to limit by 2).foreach(n => {
       if(isPrimeMap.getOrElse(n, true)) {
         primes += n
-        for(composite <- n*2 to limit by n) {
-          isPrimeMap(composite) = false
-        }
+
+        (n*2 to limit by n).foreach(c => {
+          isPrimeMap(c) = false
+        })
       }
     })
 
