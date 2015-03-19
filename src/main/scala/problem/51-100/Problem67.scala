256280c31b14dd50963e6551e5199343cfea3cb3
Keith Blaha <keith.blaha@gmail.com>
2015-03-19 11:26:49 -0700

    67 can really just extend 18

--- src/main/scala/problem/51-100/Problem67.scala
+++ src/main/scala/problem/51-100/Problem67.scala
@@ -1,8 +1,4 @@
 package com.keithblaha.euler.problem
 
-import com.keithblaha.euler.util.NumberTriangle
-
-class Problem67 extends EulerProblem {
-  override def solution = new NumberTriangle(resource.mkString).maxPath
-}
+class Problem67 extends Problem18 {}
 
