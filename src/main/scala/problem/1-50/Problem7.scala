5c4ee3bb5f52bc2e4abb83a9cab9f6897bbf0ad1
Keith Blaha <keith.blaha@gmail.com>
2015-04-17 17:48:11 -0700

    update 7

--- src/main/scala/problem/1-50/Problem7.scala
+++ src/main/scala/problem/1-50/Problem7.scala
@@ -3,8 +3,6 @@ package com.keithblaha.euler.problem
 import com.keithblaha.euler.util.Sequences._
 
 class Problem7 extends EulerProblem {
-  val primes = primesTo(1000000).toSeq.sorted
-
-  override def solution = primes(10000)
+  override def solution = primesTo(1000000)(10000)
 }
 
