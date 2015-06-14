9676b210cd5aa484bcc21cbcf2745d674c11790d
Keith Blaha <keith.blaha@gmail.com>
2015-06-14 08:22:53 -0700

    more elegance wow

--- src/main/scala/problem/1-50/Problem32.scala
+++ src/main/scala/problem/1-50/Problem32.scala
@@ -4,9 +4,7 @@ class Problem32 extends EulerProblem {
   override def solution = {
     (for {
       s <- "123456789".permutations
-      p <- Seq((1, 5), (2, 5))
-      n = p._1
-      m = p._2
+      (n, m) <- Seq((1, 5), (2, 5))
       multiplicand = s.substring(0, n).toInt
       multiplier = s.substring(n, m).toInt
       product = multiplicand * multiplier
