ebd9125b3cf0bc4da5c310871ba0fa147f54c93a
Keith Blaha <keith.blaha@gmail.com>
2015-06-15 10:59:01 -0700

    correct language

--- src/main/scala/problem/1-50/Problem32.scala
+++ src/main/scala/problem/1-50/Problem32.scala
@@ -5,9 +5,9 @@ class Problem32 extends EulerProblem {
     (for {
       s <- "123456789".permutations
       (n, m) <- Seq((1, 5), (2, 5))
-      multiplicand = s.substring(0, n).toInt
-      multiplier = s.substring(n, m).toInt
-      product = multiplicand * multiplier
+      multiplier = s.substring(0, n).toInt
+      multiplicand = s.substring(n, m).toInt
+      product = multiplier * multiplicand
       if product == s.substring(m, 9).toInt
     } yield product).toSet.sum
   }
